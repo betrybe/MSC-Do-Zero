dfcafdaa4ae54a865678ece1d451e41859a90b11
“PedroMCHenrique” <pedro.876.ph@gmail.com>
2023-01-10 12:01:16 -0300

    template msc-do-zero

--- database.sql
+++ database.sql
@@ -2,12 +2,26 @@ DROP DATABASE IF EXISTS `exercises`;
 
 CREATE DATABASE `exercises`;
 
+CREATE TABLE `exercises`.`offices` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)) ENGINE=INNODB;
+
 CREATE TABLE `exercises`.`employees` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
   `office` INT NOT NULL,
+  FOREIGN KEY (`office`) REFERENCES `offices`(`id`),
   PRIMARY KEY (`id`)) ENGINE=INNODB;
+
+
+INSERT INTO `exercises`.`offices`
+(`id`,
+`name`)
+VALUES
+(1, 'Dunder Mifflin Paper Company'),
+(2, 'Trybe');
 
 INSERT INTO `exercises`.`employees`
 (`id`,
@@ -18,4 +32,4 @@ VALUES
 (1,
 'michael',
 'scott',
-999);
+1);
